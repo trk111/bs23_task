79e061df4917562bdad8cbf0db133be087828bb0
Iventurebd <git@iventurebd.com>
2023-03-11 16:42:54 +0600

    code refactor

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,22 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Model\Category;
-use App\Model\CatetoryRelations;
-use http\Env\Request;
-use Illuminate\Routing\Route;
-use Illuminate\Support\Facades\DB;
 
 
 class TaskController extends Controller
 {
     public function task1()
     {
-        $categories = Category::select('category.Name as category_name', DB::raw('count(Item.Id) as total_items'))
-            ->leftJoin('Item_category_relations', 'category.Id', '=', 'item_category_relations.categoryId')
-            ->leftJoin('Item', 'Item_category_relations.ItemNumber', '=', 'Item.Number')
-            ->groupBy('category.Id','category_name')
-            ->orderByDesc('total_items')
-            ->get();
+        $categories = Category::category_wise_count();
 
         return view('tasks/task1',compact( 'categories'));
     }
--- app/Model/Category.php
+++ app/Model/Category.php
@@ -4,6 +4,7 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
 
 class Category extends Model
 {
@@ -24,5 +25,17 @@ class Category extends Model
 
     public function childs(){
         return $this->hasMany(CatetoryRelations::class,'ParentcategoryId','Id')->with('category');
+    }
+
+    public static function category_wise_count()
+    {
+        $result = Category::select('category.Name as category_name', DB::raw('count(Item.Id) as total_items'))
+            ->leftJoin('Item_category_relations', 'category.Id', '=', 'item_category_relations.categoryId')
+            ->leftJoin('Item', 'Item_category_relations.ItemNumber', '=', 'Item.Number')
+            ->groupBy('category.Id','category_name')
+            ->orderByDesc('total_items')
+            ->get();
+
+        return $result;
     }
 }
